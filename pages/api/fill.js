3654e9098f149e04b1f7ce29dbcf52aa62f1741f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-16 18:07:25 +0200

    database functional and filled and connected

--- pages/api/fill.js
+++ pages/api/fill.js
@@ -2,17 +2,14 @@
 import dbConnect from "../../db/connect";
 import Exercise from "../../db/Schema/Exercise";
 import Workout from "../../db/Schema/Workout";
+import libExercises from "@/lib/exercises";
+import libWorkouts from "@/lib/workouts";
 
 export default async function handler(_, res) {
   await dbConnect();
+  const workouts = Workout.create(libWorkouts);
+  const exercises = Exercise.create(libExercises);
 
-  const array = workout.exercises.map((exercise) => exercise.exerciseId);
-  const workoutName = workout.name;
-
-  //$in -- is some Mongo Magic array mapping
-  const exercises = await Exercise.where({ id: { $in: array } });
-  const exerciseNames = exercises.map((exercise) => exercise.name);
-
-  res.status(200).json({ exerciseNames, workoutName });
+  res.status(200).json({ workouts, exercises });
   return;
 }
