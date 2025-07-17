4302bb1ccfd9c96851fc2e0b635b585b8753b4a1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-17 21:07:34 +0200

    workouts get logged in browser

--- components/Workouts/WorkoutsList/index.jsx
+++ components/Workouts/WorkoutsList/index.jsx
@@ -1,7 +1,11 @@
 import List from "@/components/List";
+import useSWR from "swr";
 
 export default function WorkoutsList() {
   // fetch cards from api
+  const { data } = useSWR(`/api/workouts/`, (url) => fetch(url).then(res => res.json()))
+  console.log(data);
+  
   const cards = [];
   return (
     <>
