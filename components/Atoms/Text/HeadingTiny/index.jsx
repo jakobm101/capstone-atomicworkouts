e2e040857a1b510dd99418a52e7726de5838d347
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-21 20:59:51 +0200

    code in wrong place

--- components/Atoms/Text/HeadingTiny/index.jsx
+++ components/Atoms/Text/HeadingTiny/index.jsx
@@ -1,14 +1,3 @@
-import styled from "styled-components";
-
 export default function HeadingTiny({ children }) {
-  return (
-    <>
-      <StyledHeadingTiny>{children}</StyledHeadingTiny>
-    </>
-  );
+  return <h6>{children}</h6>;
 }
-
-const StyledHeadingTiny = styled.h6`
-  position: fixed;
-  top: 40px;
-`;
