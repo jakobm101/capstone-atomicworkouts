5cd9898bfb4664ae756d686e83e73e4abf022c0f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-22 18:06:56 +0200

    styles for the vibes

--- styles.js
+++ styles.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import { spaceMono } from "@/lib/fonts";
 
 export default createGlobalStyle`
+  :root {
+    ${spaceMono.variable && `--font-space-mono: ${spaceMono.variable};`}
+  }
+  
   *,
   *::before,
   *::after {
@@ -9,8 +14,24 @@ export default createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: system-ui;
+    font-family: var(--font-space-mono), monospace;
+   font-weight: 100;
+    letter-spacing: 0.7px;
+    color: rgb(69%, 35%, 0%);
+    text-shadow: 
+    0 0 2px rgba(69%, 35%, 0%, 0.9),
+    0 0 4px rgba(69%, 35%, 0%, 0.5),
+    0 0 8px rgba(69%, 35%, 0%, 0.5),
+    0 0 16px rgba(69%, 35%, 0%, 0.5);
     background-color: #000;
+    background-image: repeating-linear-gradient(
+    to bottom,
+    rgba(69%, 35%, 0%, 0.15), 
+    rgba(69%, 35%, 0%, 0.05) 1px,
+    transparent 1px,
+    transparent 4px
+  );
+;
   }
 
 
@@ -18,7 +39,7 @@ main {
   width: 400px;
   height: 90vh;
   margin: auto;
-margin-top: 5vh;
+  margin-top: 5vh;
   color: white;
   border-radius: 20px;
   border: grey 1px solid;
