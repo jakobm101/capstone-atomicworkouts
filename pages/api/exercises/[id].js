f6f60d535f63fc653fe8bae6858cb2f6d5a83d8f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-23 16:15:50 +0200

    error handling update

--- pages/api/exercises/[id].js
+++ pages/api/exercises/[id].js
@@ -1,15 +1,22 @@
 import dbConnect from "@/db/connect";
 import Exercise from "@/db/Schema/Exercise";
+import mongoose from "mongoose";
 
 export default async function handler(req, res) {
   try {
     await dbConnect();
-    const exercises = await Exercise.findById(req.query.id);
+    const { id } = req.query;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Exercise not found." });
+    }
+    const exercises = await Exercise.findById(id);
 
-    res.status(200).json(exercises);
-    return;
+    if (!exercises) {
+      return res.status(404).json({ error: "Exercise not found." });
+    }
+
+    return res.status(200).json(exercises);
   } catch (error) {
-    res.status(500).json({ error: error.message });
-    return;
+    return res.status(500).json({ error: error.message });
   }
 }
