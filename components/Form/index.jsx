5dd9472bab6341bc34e4f3c8bb1c0e430fd059ad
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-25 13:51:57 +0200

    form submission leads to push to /

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -3,14 +3,14 @@ import Card__AddExercise from "./CardAddExercise";
 import Form__Input from "./Input";
 import { useState } from "react";
 import ButtonClose from "../Button/ButtonClose";
-import useSWR from "swr";
 import { uid } from "uid";
 import styled from "styled-components";
 import Card from "../Atoms/Card";
 import HeadingTiny from "../Atoms/Text/HeadingTiny";
+import { useRouter } from "next/router";
 
 export default function Form() {
-  const { mutate } = useSWR(`/api/workouts`);
+  const { push } = useRouter();
   //preparing for updating {id, rep, set}
   const [exercises, setExercises] = useState([{ tempId: 1 }, { tempId: 2 }]);
 
@@ -41,9 +41,9 @@ export default function Form() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(workout),
     });
-    if (response.ok) mutate();
 
     event.target.reset();
+    push(`/`);
   };
 
   return (
