d8f4a97f4052bc1e5e0d85a34aa3c470968295a1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-01 15:40:05 +0200

    emptied new form file

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import WorkoutCard from "../Workouts/WorkoutCard";
+import SimpleWorkoutDisplay from "../Form2/SimpleWorkoutDisplay";
 
 /////////////////////////////////
 export default function Form() {
@@ -8,38 +8,12 @@ export default function Form() {
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading data.</p>;
 
-  // trying to use data
-  const baselineWorkout = data.workouts[0];
-  const baselineExerciseId = baselineWorkout.exercises[0].exerciseId;
-  const baselineExercise = data.exercises.find(
-    (exercise) => exercise._id === baselineExerciseId
-  );
 
-  // creating default workout
-  const newWorkout = { ...data.workouts[0], name: "New Workout" };
-  console.log("API Response:", newWorkout);
-  
-  // finding specific workout
-  const workoutDisplayed = data.workouts.find(
-    (workout) => workout._id === "68834cb36f8968f2fa09c973"
-  );
-  
-  
 
   ///////////////////////////////// JSX
   return (
     <>
-      <h2>{workoutDisplayed.name}</h2>
-      <ul>
-        {workoutDisplayed.exercises.map((exerciseInWorkout, index) => {
-          const idInWorkouts = exerciseInWorkout.exerciseId;
-          const exerciseInCollection = data.exercises.find(
-            (exerciseInCollection) => exerciseInCollection._id === idInWorkouts
-          );
-          return <li key={index}>{exerciseInCollection.name}</li>;
-        })}
-      </ul>
-      <WorkoutCard />
+      <SimpleWorkoutDisplay data={data} />
     </>
   );
 }
---
+++ components/Form2/SimpleWorkoutDisplay.jsx
@@ -0,0 +1,20 @@
+export default function SimpleWorkoutDisplay({ data }) {
+  const workoutDisplayed = data.workouts.find(
+    (workout) => workout._id === "68834cb36f8968f2fa09c973"
+  );
+  /////////////////////////////////
+  return (
+    <>
+      <h2>{workoutDisplayed.name}</h2>
+      <ul>
+        {workoutDisplayed.exercises.map((exerciseInWorkout, index) => {
+          const idInWorkouts = exerciseInWorkout.exerciseId;
+          const exerciseInCollection = data.exercises.find(
+            (exerciseInCollection) => exerciseInCollection._id === idInWorkouts
+          );
+          return <li key={index}>{exerciseInCollection.name}</li>;
+        })}
+      </ul>
+    </>
+  );
+}
