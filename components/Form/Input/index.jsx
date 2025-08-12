5eedd403d22b142d61c49a3adc0e58a847c22824
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-12 15:14:04 +0200

    resetting front end

--- components/Form/Input/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Form__Input({ name, children = "", type = "text", ...props }) {
-  return (
-    <>
-      <label htmlFor={name}>{children}</label>
-      <input name={name} type={type} id={name} placeholder={name} {...props}/>
-    </>
-  );
-}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,7 @@
-import Heading from "@/components/Atoms/Text/Heading";
-import WorkoutsList from "@/components/Workouts/WorkoutsList";
-import Link from "next/link";
-import styled from "styled-components";
-
 export default function HomePage() {
   return (
     <main>
-    <Link href='/exercises'>Exercises</Link>
-      <Header>atomic workouts</Header>
-      <Heading>Catalogue</Heading>
-      <WorkoutsList />
+      <h2>Hello World</h2>
     </main>
   );
 }
-
-const Header = styled.h3`
-  position: sticky;
-  font-size: 8px;
-  top: 10px;
-  left: 10px;
-  z-index: 10;
-`;
