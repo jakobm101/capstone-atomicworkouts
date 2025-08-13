493ca6c9c1a83168c8696a72b302cd6860712f1e
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-13 15:39:10 +0200

    init

--- pages/workouts/create.jsx
+++ pages/workouts/create.jsx
@@ -1,7 +1,17 @@
 export default function WorkoutCreate() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log("Submitting", data);
+  };
   return (
     <main>
       <h1>Creating Workout</h1>
+      <form onSubmit={handleSubmit}>
+      <input type="text" name="name" placeholder="enter workout name" />
+        <button type="submit">submit</button>
+      </form>
     </main>
   );
 }
