80b66f68d731f410f5822874a0c42574b35e85e1
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-13 13:34:02 +0200

    exercise details page done

--- pages/exercises/[id].jsx
+++ pages/exercises/[id].jsx
@@ -1,7 +1,43 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
 export default function ExerciseDetailsPage() {
+  const router = useRouter();
+  const id = router.query.id;
+  const { data: exercise, isLoading, error } = useSWR(`/api/exercises/${id}`);
+  if (isLoading || error) {
+    return (
+      <main>
+        <h1>{error ? `error` : `loading`}</h1>
+      </main>
+    );
+  }
+  const { name, instructions, muscleGroups, _id } = exercise;
+
   return (
     <main>
       <h1>Exercise Details</h1>
+      <h2>{name}</h2>
+      <h3>Instructions</h3>
+      {instructions.map((step, index) => {
+        return (
+          <div key={step}>
+            <h4>{index + 1}</h4>
+            <p>{step}</p>
+          </div>
+        );
+      })}
+      <h3>Muscle Groups</h3>
+      <ul>
+        {muscleGroups.map((muscle) => {
+          return <li key={muscle}>{muscle}</li>;
+        })}
+      </ul>
+      <p>————————</p>
+      <Link href={`/`}>home</Link>
+      <Link href={`/exercises`}>exercises</Link>
+      <Link href={`/workouts`}>workouts</Link>
     </main>
   );
 }
