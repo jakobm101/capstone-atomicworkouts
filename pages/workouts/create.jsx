7a5226e9db45462e8468156b7a4fc01063eba360
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-13 15:54:17 +0200

    setting name in preview works

--- pages/workouts/create.jsx
+++ pages/workouts/create.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 export default function WorkoutCreate() {
+  const [workoutPreview, setWorkoutPreview] = useState({
+    workoutName: "Preview Workout",
+    exercises: [{}, {}],
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData.entries());
     console.log("Submitting", data);
+    setWorkoutPreview({ ...workoutPreview, ...data });
   };
+
+  console.log("workout preview", workoutPreview);
+
   return (
     <main>
       <h1>Creating Workout</h1>
       <form onSubmit={handleSubmit}>
-      <input type="text" name="name" placeholder="enter workout name" />
+        <input
+          type="text"
+          name="workoutName"
+          placeholder="enter workout name"
+        />
         <button type="submit">submit</button>
       </form>
+
+      <h2>Preview</h2>
+      <h3>{workoutPreview.workoutName}</h3>
     </main>
   );
 }
