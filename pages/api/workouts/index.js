f2674563626bfcf29a9d34532e133f83d7d6dd35
Jakob-M-101 <20402329+jakobm101@users.noreply.github.com>
2025-08-13 12:17:19 +0200

    Update pages/api/workouts/index.js
    Co-authored-by: Stille Klaus <mail@klausstille.com>

--- pages/api/workouts/index.js
+++ pages/api/workouts/index.js
@@ -11,8 +11,7 @@ export default async function handler(req, res) {
       await dbConnect();
       const workouts = await Workout.find().populate("exercises.exercise");
 
-      res.status(200).json(workouts);
-      return;
+      return res.status(200).json(workouts);
     } catch (error) {
       res.status(500).json({ error: error.message });
       return;
