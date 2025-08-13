7039c5e9aa0606003a19543f6d711c2050bc8120
Jakob-M-101 <20402329+jakobm101@users.noreply.github.com>
2025-08-13 12:17:26 +0200

    Update pages/api/workouts/index.js
    Co-authored-by: Stille Klaus <mail@klausstille.com>

--- pages/api/workouts/index.js
+++ pages/api/workouts/index.js
@@ -22,8 +22,7 @@ export default async function handler(req, res) {
     try {
       await dbConnect();
       await Workout.create(req.body);
-      res.status(200).json({ message: "posting" });
-      return;
+      return res.status(200).json({ message: "posting" });
     } catch (error) {
       console.log("error posting", error.message);
 
