6d0153b4b79140a95b5da57967de9f46727bd8b0
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-14 23:26:44 +0200

    styled

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Header() {
-  return <Link href={`/`}>Atomic Workouts</Link>;
+  return <StyledLink href={`/`}>Atomic Workouts</StyledLink>;
 }
+
+const StyledLink = styled(Link)`
+  font-size: xx-small;
+`;
