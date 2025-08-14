d5067dd3c6cb831af105d43bcb715b805fc320c3
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-15 00:01:51 +0200

    styles

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -13,4 +13,15 @@ export default function Navigation() {
 
 const StyledDiv = styled.div`
   border-top: 1px solid var(--color-orange-5);
+  padding: 20px;
+  margin: -20px;
+  position: sticky;
+  left: 0;
+  right: 0;
+  bottom: -20px;
+  background-color: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(2px);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
