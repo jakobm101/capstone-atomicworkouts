c1336504f1505cfe80f9a2a3b88e51942e4d33cd
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-15 12:28:05 +0200

    card as styles created for workouts page

--- pages/workouts/index.jsx
+++ pages/workouts/index.jsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/Layout";
 import { collectMuscleGroups } from "@/lib/utils";
+import { Plus } from "lucide-react";
 import Link from "next/link";
+import styled from "styled-components";
 import useSWR from "swr";
 
 export default function WorkoutsPage() {
@@ -24,11 +26,15 @@ export default function WorkoutsPage() {
   return (
     <Layout>
       <h2>Workouts</h2>
+      <StyledLink href={`/workouts/create`}>
+        <Plus />
+        Add New Workout
+      </StyledLink>
       {workouts.map((workout) => {
         const muscleGroupsInWorkout = collectMuscleGroups(workout.exercises);
 
         return (
-          <div key={workout._id}>
+          <StyledDiv key={workout._id}>
             <h3>{workout.name}</h3>
 
             <h4>Exercises</h4>
@@ -45,11 +51,33 @@ export default function WorkoutsPage() {
               ))}
             </ul>
             <Link href={`/workouts/${workout._id}`}>Details</Link>
-            <p>—————————————————————————</p>
-          </div>
+          </StyledDiv>
         );
       })}
-      <Link href={`/workouts/create`}>add new workout</Link>
+      <StyledLink href={`/workouts/create`}>
+        <Plus />
+        Add New Workout
+      </StyledLink>
     </Layout>
   );
 }
+
+const StyledDiv = styled.div`
+  border: 1px solid var(--color-orange-5);
+  border-radius: 4px;
+  padding: 0 0 10px 10px;
+  margin-bottom: 10px;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column wrap;
+  padding: 32px;
+  margin-bottom: 10px;
+  width: 100%;
+
+  border: var(--color-orange-5) 1px solid;
+  border-radius: 4px;
+`;
