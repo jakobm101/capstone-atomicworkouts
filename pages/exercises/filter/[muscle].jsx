efdadb2db82d4c092a2d2c4e8cd13517b605317f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-08-15 14:00:15 +0200

    backend filter on front end page

--- pages/exercises/filter/[muscle].jsx
+++ pages/exercises/filter/[muscle].jsx
@@ -1,17 +1,27 @@
 import Card from "@/components/Card";
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import useSWR from "swr";
 
 export default function FilteredExercisesPage() {
-  const { data: exercises, isLoading, error } = useSWR(`/api/exercises`);
+  const router = useRouter();
+  const muscle = router.query.muscle;
+  if (!muscle) {
+    return <Layout>loading</Layout>;
+  }
+
+  const {
+    data: exercises,
+    isLoading,
+    error,
+  } = useSWR(`/api/exercises/filter/${muscle}`);
 
   if (isLoading) {
-    return (
-      <Layout>
-        <h2>isLoading</h2>
-      </Layout>
-    );
+    return;
+    <Layout>
+      <h2>isLoading</h2>
+    </Layout>;
   }
   if (error) {
     <Layout>
@@ -22,6 +32,7 @@ export default function FilteredExercisesPage() {
   return (
     <Layout>
       <h2>Exercises</h2>
+      <h3>Filtered by {muscle}</h3>
       {exercises.map((exercise) => {
         return (
           <Card key={exercise._id}>
