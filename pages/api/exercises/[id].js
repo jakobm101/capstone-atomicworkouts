d2c13979224486bfc5f338481e8176814e38341c
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2026-03-11 16:51:54 +0100

    added temporary serverside next auth demo

--- pages/api/exercises/[id].js
+++ pages/api/exercises/[id].js
@@ -1,21 +1,28 @@
 import dbConnect from "@/db/connect";
 import Exercise from "@/db/Schema/Exercise";
 import mongoose from "mongoose";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
   try {
-    await dbConnect();
-    const { id } = req.query;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ error: "Exercise not found." });
-    }
-    const exercises = await Exercise.findById(id);
+    if (session) {
+      await dbConnect();
+      const { id } = req.query;
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "Exercise not found." });
+      }
+      const exercise = await Exercise.findById(id);
 
-    if (!exercises) {
-      return res.status(404).json({ error: "Exercise not found." });
-    }
+      if (!exercise) {
+        return res.status(404).json({ error: "Exercise not found." });
+      }
 
-    return res.status(200).json(exercises);
+      return res.status(200).json(exercise);
+    } else {
+      return res.status(401).json({ status: "Not authorized" });
+    }
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- pages/exercises/[id].jsx
+++ pages/exercises/[id].jsx
@@ -2,15 +2,23 @@ import Layout from "@/components/Layout";
 import MuscleGroups from "@/components/MuscleGroups";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import { useSession } from "next-auth/react";
 
 export default function ExerciseDetailsPage() {
+  const { data: session } = useSession();
   const router = useRouter();
   const id = router.query.id;
   const { data: exercise, isLoading, error } = useSWR(`/api/exercises/${id}`);
-  if (isLoading || error) {
+  if (isLoading || error || !session) {
     return (
       <Layout>
-        <h1>{error ? `error` : `loading`}</h1>
+        <h1>
+          {!session
+            ? "Please log in to view exercise details (next auth demo)"
+            : isLoading
+              ? `loading`
+              : `error`}
+        </h1>
       </Layout>
     );
   }
