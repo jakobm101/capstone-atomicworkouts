f8f6ef8fc272d32e77fbd9666fc1ea15cce5795c
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2026-03-12 15:52:21 +0100

    owner for workouts established

--- pages/api/workouts/index.js
+++ pages/api/workouts/index.js
@@ -2,14 +2,18 @@ import dbConnect from "@/db/connect";
 import Workout from "@/db/Schema/Workout";
 // necessary import for populate:
 import Exercise from "@/db/Schema/Exercise";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(req, res) {
-
+  const token = await getToken({ req: req });
+  const userId = token?.sub;
   ///////////////////////////////// GET
   if (req.method === "GET") {
     try {
       await dbConnect();
-      const workouts = await Workout.find().populate("exercises.exercise");
+      const workouts = await Workout.find({ owner: userId }).populate(
+        "exercises.exercise"
+      );
 
       return res.status(200).json(workouts);
     } catch (error) {
@@ -21,7 +25,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       await dbConnect();
-      await Workout.create(req.body);
+      await Workout.create({ ...req.body, owner: userId });
       return res.status(200).json({ message: "posting" });
     } catch (error) {
       console.log("error posting", error.message);
