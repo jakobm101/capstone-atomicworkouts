856122a9b3d8a848369cab4509bd214c7c010015
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2026-03-12 21:45:20 +0100

    fishy credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,42 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
- 
+import CredentialsProvider from "next-auth/providers/credentials";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "username",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials) {
+            if (
+              credentials.username === "fisch" &&
+              credentials.password === "fisch"
+            ) {
+              return {
+                name: "Neuer Fisch",
+                email: "test@example.com",
+                id: "a1b2c3d4",
+              };
+            } else {
+              return null;
+            }
+          },
+        })
+      : GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
     // ...add more providers here
   ],
 };
- 
-export default NextAuth(authOptions)
+
+export default NextAuth(authOptions);
